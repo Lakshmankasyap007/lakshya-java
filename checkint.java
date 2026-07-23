ec01fc0d8b756a72f7ec09d4b14580b16b7b8d06
Luckyjaiswal <155840324+Luckyjaiswa@users.noreply.github.com>
2026-07-23 22:55:55 +0530

    Update

--- checkint.java
+++ checkint.java
@@ -1,8 +1,5 @@
 import java.util.Scanner;
 public class checkint{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your number :");
-        System.out.println(sc.hasNextInt());
-}
+asnbahsbhh
 }
